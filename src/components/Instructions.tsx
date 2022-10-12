c1e85409bb916429ae306282a7509bda99c7a976
Jerrica Bobadilla <jbobadilla27@gmail.com>
2022-10-12 09:57:53 -0700

    configure for ts

--- src/components/Instructions.tsx
+++ src/components/Instructions.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 function Instructions() {
   return <p className="instructions">Select a note.</p>;
 }
--- src/components/NoteContainer.tsx
+++ src/components/NoteContainer.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Search from "./Search";
 import Sidebar from "./Sidebar";
 import Content from "./Content";
